0b0b4a86a1823299f451aee16b126658e6e57b3c
Geoffrey Matthews <gmatthews@wlu.edu>
2023-01-25 11:01:55 -0500

    read.py

--- homework/hw03/read.py
+++ homework/hw03/read.py
@@ -9,29 +9,28 @@ import string
 debug = False
 
 def read(s):
-    return parse(list(s))
+    return read_sexpr(list(s))
 
 punctuation = string.whitespace + '()'
-
 
 def skip_blanks(s):
     while s[0] in string.whitespace:
         s.pop(0)
 
-def parse(s):
+def read_sexpr(s):
     global debug
     if debug:
-        print('parse:', ''.join(s))
+        print('sread:', ''.join(s))
     skip_blanks(s)
     if s[0] in string.digits:
-        return parse_number(s)
+        return read_number(s)
     elif s[0] not in punctuation:
-        return parse_symbol(s)
+        return read_symbol(s)
     elif s[0] == '(':
         s.pop(0)
-        return parse_list(s)
+        return read_list(s)
 
-def parse_number(s):
+def read_number(s):
     global debug
     if debug:
         print('parse_number:', ''.join(s))
@@ -41,7 +40,7 @@ def parse_number(s):
         n = n*10 + int(s.pop(0))
     return n
 
-def parse_symbol(s):
+def read_symbol(s):
     global debug
     if debug:
         print('parse_symbol:', ''.join(s))
@@ -51,7 +50,7 @@ def parse_symbol(s):
         w += s.pop(0)
     return w
 
-def parse_list(s):
+def read_list(s):
     global debug
     if debug:
         print('parse_list:', ''.join(s))
@@ -59,8 +58,8 @@ def parse_list(s):
     if s[0] == ')':
         s.pop(0)
         return []
-    car = parse(s)
-    cdr = parse_list(s)
+    car = read_sexpr(s)
+    cdr = read_list(s)
     return [car] + cdr
 
 if __name__ == '__main__':
