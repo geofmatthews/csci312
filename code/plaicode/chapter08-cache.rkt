c9c4de08091132a07ced9d9bb24bb9a54a74b784
Matthews <gmatthews@wlu.edu>
2023-02-15 12:15:25 -0500

    code chap 8

--- code/plaicode/chapter08-cache.rkt
+++ code/plaicode/chapter08-cache.rkt
@@ -1,18 +1,14 @@
 #lang plai
 
+;; From previous interpreter
 (define-type CFAE/L
   [num (n number?)]
   [add (lhs CFAE/L?) (rhs CFAE/L?)]
   [id (name symbol?)]
   [fun (param symbol?) (body CFAE/L?)]
   [app (fun-expr CFAE/L?) (arg-expr CFAE/L?)])
-(define-type CFAE/L-Value
-  [numV (n number?)]
-  [closureV (param symbol?)
-            (body CFAE/L?)
-            (env Env?)]
-  [exprV (expr CFAE/L?)
-         (env Env?)])
+
+
 (define-type Env
   [mtSub]
   [aSub (name symbol?) (value CFAE/L-Value?) (env Env?)])
@@ -24,22 +20,43 @@
           (if (symbol=? bound-name name)
               bound-value
               (lookup name rest-env))]))
-
 ;; num+ : CFAE/L-Value CFAE/L-Value -! numV
 (define (num+ n1 n2)
   (numV (+ (numV-n (strict n1)) (numV-n (strict n2)))))
 ;; num-zero? : CFAE/L-Value !boolean
 (define (num-zero? n)
   (zero? (numV-n (strict n))))
-;; strict : CFAE/L-Value ! CFAE/L-Value [excluding exprV]
+
+;; New for this interpreter:
+(define-type CFAE/L-Value
+  [numV (n number?)]
+  [closureV (param symbol?)
+            (body CFAE/L?)
+            (env Env?)]
+  [exprV (expr CFAE/L?)
+         (env Env?)
+         (cache boxed-boolean/CFAE/L-Value?)])
+(define (boxed-boolean/CFAE/L-Value? v)
+  (and (box? v)
+       (or (boolean? (unbox v))
+           (numV? (unbox v))
+           (closureV? (unbox v)))))
+;; strict : CFAE/L-Value !CFAE/L-Value [excluding exprV]
 (define (strict e)
   (type-case CFAE/L-Value e
-    [exprV (expr env)
-           (local ([define the-value (strict (interp expr env))])
-             (begin
-               (printf "Forcing exprV to ~a \n" the-value)
-               the-value))]
+    [exprV (expr env cache)
+           (if (boolean? (unbox cache))
+               (local [(define the-value (strict (interp expr env)))]
+                 (begin
+                   (printf "Forcing exprV ~a to ~a\n" expr the-value)
+                   (set-box! cache the-value)
+                   the-value))
+               (local [(define the-value (unbox cache))]
+                 (begin
+                   (printf "Using cached value ~a\n" the-value)
+                   the-value)))]
     [else e]))
+
 ;; interp : CFAE/L Env !CFAE/L-Value
 (define (interp expr env)
   (type-case CFAE/L expr
@@ -50,18 +67,19 @@
          (closureV bound-id bound-body env)]
     [app (fun-expr arg-expr)
          (local ([define fun-val (strict (interp fun-expr env))]
-                 [define arg-val (exprV arg-expr env)])
+                 [define arg-val (exprV arg-expr env (box false))])
            (interp (closureV-body fun-val)
                    (aSub (closureV-param fun-val)
                          arg-val
                          (closureV-env fun-val))))]))
+
 
 (test (interp (add (num 3) (num 3)) (mtSub))
       (numV 6))
 
 (test (interp
        (app (fun 'x (add (id 'x) (id 'x)))
-           (num 3)) (mtSub))
+            (num 3)) (mtSub))
       (numV 6))
 
 (test (interp (app (fun 'x (num 3))
---
+++ code/plaicode/chapter10.rkt
@@ -0,0 +1,104 @@
+#lang plai
+
+(define-type RCFAE
+  [num (n number?)]
+  [add (lhs RCFAE?) (rhs RCFAE?)]
+  [mult (lhs RCFAE?) (rhs RCFAE?)]
+  [id (name symbol?)]
+  [fun (param symbol?) (body RCFAE?)]
+  [app (fun-expr RCFAE?) (arg-expr RCFAE?)]
+  [if0 (text-expr RCFAE?)
+       (true-expr RCFAE?)
+       (false-expr RCFAE?)]
+  [rec (bound-id symbol?)
+    (named-expr RCFAE?)
+    (bound-body RCFAE?)]
+  )
+
+(define (num+ lhs rhs)
+  (+ (num-n lhs) (num-n rhs)))
+
+(define (num* lhs rhs)
+  (* (num-n lhs) (num-n rhs)))
+
+(define (num-zero? num)
+  (= 0 (num-n num)))
+
+
+(define-type RCFAE-Value
+  [numV (n number?)]
+  [closureV (param symbol?)
+            (body RCFAE?)
+            (env Env?)])
+(define (boxed-RCFAE-Value? v)
+  (and (box? v)
+       (RCFAE-Value? (unbox v))))
+(define-type Env
+  [mtSub]
+  [aSub (name symbol?)
+        (value RCFAE-Value?)
+        (env Env?)]
+  [aRecSub (name symbol?)
+           (value boxed-RCFAE-Value?)
+           (env Env?)])
+;; lookup : symbol env! RCFAE-Value
+(define (lookup name env)
+  (type-case Env env
+    [mtSub ()
+           (error name "no binding for identifier")]
+    [aSub (bound-name bound-value rest-env)
+          (if (symbol=? bound-name name)
+              bound-value
+              (lookup name rest-env))]
+    [aRecSub (bound-name boxed-bound-value rest-env)
+             (if (symbol=? bound-name name)
+                 (unbox boxed-bound-value)
+                 (lookup name rest-env))]))
+;; cyclically-bind-and-interp : symbol RCFAE env! env
+(define (cyclically-bind-and-interp bound-id named-expr env)
+  (local ([define value-holder (box (numV 1729))]
+          [define new-env (aRecSub bound-id value-holder env)]
+          [define named-expr-val (interp named-expr new-env)])
+    (begin
+      (set-box! value-holder named-expr-val)
+      new-env)))
+
+
+;; interp : RCFAE env! RCFAE-Value
+(define (interp expr env)
+  (type-case RCFAE expr
+    [num (n) (numV n)]
+    [add (l r) (num+ (interp l env) (interp r env))]
+    [mult (l r) (num* (interp l env) (interp r env))]
+    [if0 (test truth falsity)
+         (if (num-zero? (interp test env))
+             (interp truth env)
+             (interp falsity env))]
+    [id (v) (lookup v env)]
+    [fun (bound-id bound-body)
+         (closureV bound-id bound-body env)]
+    [app (fun-expr arg-expr)
+         (local ([define fun-val (interp fun-expr env)])
+           (interp (closureV-body fun-val)
+                   (aSub (closureV-param fun-val)
+                         (interp arg-expr env)
+                         (closureV-env fun-val))))]
+    [rec (bound-id named-expr bound-body)
+      (interp bound-body
+              (cyclically-bind-and-interp bound-id
+                                          named-expr
+                                          env))]))
+
+
+
+(test (interp
+       (rec 'fac
+         (fun 'n
+              (if0 (id 'n) (num 1)
+                   (mult (id 'n) (app (id 'fac)
+                                      (add (id 'n)
+                                           (num -1))))))
+         (app (id 'fac) (num 5)))
+              (mtSub))
+         (numV 120))
+       
